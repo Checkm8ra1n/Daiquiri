f34101d2ec9a6154c20b2f7f2b0f14dd13dd0424
Checkm8ra1n <superfrancybros@outlook.com>
2025-07-10 13:01:47 +0200

    Initial Commit

---
+++ Daiquiri/DaiquiriApp.swift
@@ -0,0 +1,17 @@
+//
+//  DaiquiriApp.swift
+//  Daiquiri
+//
+//  Created by Checkm8Croft on 10/07/25.
+//
+
+import SwiftUI
+
+@main
+struct DaiquiriApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
